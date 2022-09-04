f16e2c79ead55ffa17749850a8f8b11e91ea71bf
gusi4ka <112167073+gusi4ka@users.noreply.github.com>
2022-09-04 20:07:35 +0300

    Add files via upload

---
+++ HomeWork03.09/Zada4a02/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Console.Write("Введи число: ");
+// int anyNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("Третья цифра" + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("Третьей цифры нет");
+}
---
+++ HomeWork03.09/zada4a03/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введи число,обозначающее день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+void Number (int dayNumber) {
+
+if (dayNumber == 6 || dayNumber ==7) {
+
+Console.WriteLine("(Выходной день)");
+}
+else
+
+    Console.WriteLine("(День не выходной)");
+}
+Number (dayNumber);
+
